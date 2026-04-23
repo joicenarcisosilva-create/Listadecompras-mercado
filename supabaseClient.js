838e2fc30ad215577cfd6b7792e217d6ac470d5d
Joice <joice.narciso.silva@escola.pr.gov.br>
2026-04-23 09:47:10 -0300

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -3,7 +3,7 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 
 // Cria o client com a URL e a chave fornecidas
 export const supabase = createClient(
-  'https://ixugmhmmnvobcnzkqygu.supabase.co',
+  'https://cghrbsoqisqiauobjbqx.supabase.co',
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNnaHJic29xaXNxaWF1b2JqYnF4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI1MzcxMjUsImV4cCI6MjA4ODExMzEyNX0.SMzIRgNPZ4sWsHpkh1FDSWJtHZp08_dAHv-dZXw6xfk'
 )
     
